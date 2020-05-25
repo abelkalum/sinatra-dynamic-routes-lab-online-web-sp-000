5f986a22de600ea43d3b4f39d5225f7f7be4e666
Abel Kalum <chumbabel@gmail.com>
2020-05-25 12:45:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,8 +23,13 @@ class App < Sinatra::Base
 	end
 	
 	get '/reversename/:name' do
-		@name = params[:name]
-		@name.reverse
+		word1 = params[:word1]
+		word2 = params[:word2]
+		word3 = params[:word3]
+		word4 = params[:word4]
+		word5 = params[:word5]
+
+		"#{word1} #{word2} #{word3} #{word4} #{word5}."
 	end
 
 end
